aaeba968e1d65080f99b85ebcbeee0b6e70af245
Ilan Biala <ilan.biala@gmail.com>
2014-02-15 01:00:38 -0500

    testing passes again

--- test/test-creation.js
+++ test/test-creation.js
@@ -4,39 +4,47 @@ var path = require('path');
 var helpers = require('yeoman-generator').test;
 
 describe('kirby generator', function () {
+	var kirby;
+
 	beforeEach(function (done) {
+		var dependencies = [
+			'../../app'
+		];
+
 		helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
 			if (err) {
 				return done(err);
 			}
 
-			this.app = helpers.createGenerator('kirby:app', [
-				'../../app'
-			]);
+			kirby = helpers.createGenerator('kirby:app', dependencies);
 			done();
 		}.bind(this));
 	});
 
 	it('creates expected files', function (done) {
+		helpers.mockPrompt(kirby, {
+			whichFolder: 'kirby-mocha'
+		});
+
 		var expected = [
 			// add files you expect to exist here.
-			'kirby/.gitignore',
-			'kirby/.htaccess',
-			'kirby/assets/',
-			'kirby/content/',
-			'kirby/index.php',
-			'kirby/kirby/',
-			'kirby/license.md',
-			'kirby/package.json',
-			'kirby/readme.md',
-			'kirby/site/'
+			'kirby-mocha/.gitignore',
+			'kirby-mocha/.htaccess',
+			'kirby-mocha/assets/',
+			'kirby-mocha/content/',
+			'kirby-mocha/index.php',
+			'kirby-mocha/kirby/',
+			'kirby-mocha/license.mdown',
+			'kirby-mocha/package.json',
+			'kirby-mocha/readme.mdown',
+			'kirby-mocha/site/'
 		];
 
-		helpers.mockPrompt(this.app, {
-			'someOption': true
-		});
-		this.app.options['skip-install'] = true;
-		this.app.run({}, function () {
+		// helpers.mockPrompt(this.app, {
+		// 	'someOption': true
+		// });
+		kirby.options['skip-install'] = true;
+		kirby.run({}, function () {
 			helpers.assertFile(expected);
 			done();
 		});
